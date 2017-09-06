1cad2c222326b4f7798ea8e4c75c46216d2b1285
Taurin <taurin.tao@discovermelody.com>
2017-09-06 12:11:52 +0800

    UIView 分类

--- DiscoverMelody/DiscoverMelody/Categories/UIView+Frame.h
+++ DiscoverMelody/DiscoverMelody/Categories/UIView+Frame.h
@@ -10,4 +10,21 @@
 
 @interface UIView (Frame)
 
+- (CGFloat)dm_x;
+- (void)setDm_x:(CGFloat)x;
+- (CGFloat)dm_y;
+- (void)setDm_y:(CGFloat)y;
+- (CGPoint)dm_origin;
+- (void)setDm_origin:(CGPoint)origin;
+- (CGFloat)dm_width;
+- (void)setDm_width:(CGFloat)width;
+- (CGFloat)dm_height;
+- (void)setDm_height:(CGFloat)height;
+- (CGSize)dm_size;
+- (void)setDm_size:(CGSize)size;
+- (CGFloat)dm_maxX;
+- (CGFloat)dm_maxY;
+- (CGFloat)dm_right;
+- (CGFloat)dm_bottom;
+
 @end
