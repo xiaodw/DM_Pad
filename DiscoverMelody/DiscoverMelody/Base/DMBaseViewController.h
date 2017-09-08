1749b7d8e23947ac485ae62d678dbe45cdd3d1ef
Ares.li <rymg2017@qq.com>
2017-09-08 11:27:51 +0800

    添加导航条用户信息

--- DiscoverMelody/DiscoverMelody/Base/DMBaseViewController.h
+++ DiscoverMelody/DiscoverMelody/Base/DMBaseViewController.h
@@ -9,7 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface DMBaseViewController : UIViewController
-//- (void)setNavTitle:(NSString *)title;
+
+@property (nonatomic, strong) UIImageView *headImageView;
+@property (nonatomic, strong) UILabel *userLabel;
+
+//导航透明
 - (void)setNavigationBarTransparence;
+
+//导航条左侧菜单点击
 - (void)clickMenuBtn:(id)sender;
+
+//设置导航条右边按钮
+- (void)setRigthBtn:(CGRect)frame
+              title:(NSString *)title
+        titileColor:(UIColor *)titleColor
+          imageName:(NSString *)imageName
+               font:(UIFont *)font;
+
+//更新导航条用户信息
+- (void)updateUserInfo;
+
 @end
