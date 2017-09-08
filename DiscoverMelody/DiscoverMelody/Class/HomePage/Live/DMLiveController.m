3cfdf8ad6154717f05e05afb382a1f1794c3b20b
Taurin <taurin.tao@discovermelody.com>
2017-09-08 12:14:37 +0800

    课程列表空数据

---
+++ DiscoverMelody/DiscoverMelody/Class/HomePage/Live/DMLiveController.m
@@ -0,0 +1,22 @@
+//
+//  DMLiveController.m
+//  DiscoverMelody
+//
+//  Created by My mac on 2017/9/8.
+//  Copyright © 2017年 Discover Melody. All rights reserved.
+//
+
+#import "DMLiveController.h"
+
+@interface DMLiveController ()
+
+@end
+
+@implementation DMLiveController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
