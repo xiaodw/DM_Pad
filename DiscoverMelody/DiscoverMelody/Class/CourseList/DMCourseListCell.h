cbabded5d93588540e0168894da547b03c3a74cf
Taurin <taurin.tao@discovermelody.com>
2017-09-13 15:28:53 +0800

    课程列表接口

--- DiscoverMelody/DiscoverMelody/Class/CourseList/DMCourseListCell.h
+++ DiscoverMelody/DiscoverMelody/Class/CourseList/DMCourseListCell.h
@@ -1,6 +1,18 @@
 #import <UIKit/UIKit.h>
 
+@class DMCourseListCell;
+
+@protocol DMCourseListCellDelegate <NSObject>
+
+@optional
+- (void)courseListCellDidTapCoursesFiles:(DMCourseListCell *)courseListCell;
+- (void)courseListCellDidTapRelook:(DMCourseListCell *)courseListCell;
+
+@end
+
 @interface DMCourseListCell : UITableViewCell
+
+@property (weak, nonatomic) id<DMCourseListCellDelegate> delegate;
 
 @property (strong, nonatomic) NSObject *model;
 
