aa2636da6cd4a1c248d2bd233491ffea063902bd
Taurin <taurin.tao@discovermelody.com>
2017-09-14 18:24:05 +0800

    底部工具条

---
+++ DiscoverMelody/DiscoverMelody/Class/HomePage/Live/Course/DMBottomBar.h
@@ -0,0 +1,21 @@
+//
+//  DMBottomBar.h
+//  DiscoverMelody
+//
+//  Created by My mac on 2017/9/14.
+//  Copyright © 2017年 Discover Melody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@class DMBottomBar;
+
+@protocol DMBottomBarDelegate <NSObject>
+
+
+@end
+
+@interface DMBottomBar : UIView
+
+@end
