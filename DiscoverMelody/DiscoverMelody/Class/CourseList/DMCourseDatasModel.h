703324c5354a87fb30990f831a81da03a41fdc77
Ares.li <rymg2017@qq.com>
2017-09-22 16:22:52 +0800

    修改课程列表Cell的问卷可点逻辑

--- DiscoverMelody/DiscoverMelody/Class/CourseList/DMCourseDatasModel.h
+++ DiscoverMelody/DiscoverMelody/Class/CourseList/DMCourseDatasModel.h
@@ -16,6 +16,6 @@
 @property (nonatomic, copy) NSString *start_time;//课程开始时间 秒
 @property (nonatomic, copy) NSString *duration;//课程时长 秒
 @property (nonatomic, copy) NSString *live_status;//0-未开始，1-上课中，2-课程结束，3-取消课程
-@property (nonatomic, copy) NSString *survey;//0-未上课未点评，1-已上课未点评，2-已上课点评过
+@property (nonatomic, copy) NSString *survey;//0 不可点，1，2 可点
 @property (nonatomic, copy) NSString *video_id;//课程回顾的id
 @end
