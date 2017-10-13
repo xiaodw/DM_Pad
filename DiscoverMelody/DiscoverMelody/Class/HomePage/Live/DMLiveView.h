7c65af303f429d0ba91998a36887669608a620ac
Taurin <taurin.tao@discovermelody.com>
2017-10-13 13:57:16 +0800

    直播房间左侧按钮们的阴影提取

--- DiscoverMelody/DiscoverMelody/Class/HomePage/Live/DMLiveView.h
+++ DiscoverMelody/DiscoverMelody/Class/HomePage/Live/DMLiveView.h
@@ -1,11 +1,23 @@
 #import <UIKit/UIKit.h>
 
+@class DMLiveView;
+
+@protocol DMLiveViewDelegate
+
+@optional
+- (void)liveViewShouldSmallDidTapView:(DMLiveView *)view;
+
+@end
+
 typedef NS_ENUM(NSInteger, DMLiveViewMode) {
-    DMLiveViewRemote, // 远端
-    DMLiveViewlocal //
+    DMLiveViewSmall, // 本地
+    DMLiveViewBalance, //
+    DMLiveViewFull // 远端
 };
 
 @interface DMLiveView : UIView
+
+@property (weak, nonatomic) id<DMLiveViewDelegate> delegate;
 
 @property (assign, nonatomic) CGFloat voiceValue;
 @property (assign, nonatomic) DMLiveViewMode mode;
